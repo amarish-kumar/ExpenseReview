d2077fa2943afe903e7b9e48fe2298ce0b303754
Rahul Sahay <rahulsahay19@hotmail.com>
2017-09-21 14:19:24 +0530

    angular improvised

--- ReimbursementApp/ClientApp/app/components/employee/employee.component.ts
+++ ReimbursementApp/ClientApp/app/components/employee/employee.component.ts
@@ -5,20 +5,20 @@ import { EmployeeService } from './employee.service';
 @Component({
     selector: 'app-employee',
     templateUrl: './employee.component.html',
-    styleUrls:['./employee.component.css'],
-    providers:[EmployeeService]
+    styleUrls:['./employee.component.css']
+   // providers:[EmployeeService]
     
 })
 
 export class EmployeeComponent implements OnInit {
     employees:any;
-    constructor(private employeeService:EmployeeService) { }
+    constructor() { }
 
     ngOnInit() {
-        this.employeeService.getEmployees().subscribe(employees => {
-            this.employees = employees;
-            console.log("Employees:- ", this.employees);
+    //     this.employeeService.getEmployees().subscribe(employees => {
+    //         this.employees = employees;
+    //         console.log("Employees:- ", this.employees);
 
-     });
+    //  });
     }
 }
