ba1e1b8cd9d53abeb12eac0d5edb3c5c784163a5
Rahul Sahay <rahulsahay19@hotmail.com>
2017-09-22 20:14:14 +0530

    Search form by id done

--- ReimbursementApp/ClientApp/app/components/search-employee/search-employee.component.ts
+++ ReimbursementApp/ClientApp/app/components/search-employee/search-employee.component.ts
@@ -12,21 +12,23 @@ import { EmployeeService } from '../../services/employee.service';
 })
 
 export class SearchEmployeeComponent implements OnInit {
-    employee: any;
+    employees;
     id: any;
+    showHide: boolean;
     @ViewChild('empIdInput') empIdInput;
     constructor(private router: Router,private employeeService: EmployeeService) { }
 
     ngOnInit() {
-
+        this.showHide = false;
     }
 
     searchEmployee() {
         this.employeeService.getEmployee(this.empIdInput.nativeElement.value)
             .subscribe(e => {
                    // console.log(this.empIdInput.nativeElement);
-                    this.employee = e;
-                    console.log("Employee Fetched:-", this.employee);
+                    this.employees = e;
+                    console.log("Employee Fetched:-", this.employees);
+                    this.showHide = true;
                 },
                 err => {
                     if (err.status == 404) {
