47d240532501fbaa56d529e03fcf95c837adb7af
Rahul Sahay <rahulsahay19@hotmail.com>
2017-10-06 11:51:39 +0530

    Modified Base URL in Detail-View

--- ReimbursementApp/ClientApp/app/components/detail-view/detail-view.component.ts
+++ ReimbursementApp/ClientApp/app/components/detail-view/detail-view.component.ts
@@ -6,7 +6,7 @@ import { DocsService } from '../../services/document.service';
 import { ToastyService } from "ng2-toasty";
 import { Expense } from './../../models/expense';
 import { NgForm } from '@angular/forms';
-import { SafeResourceUrl,DomSanitizer } from '@angular/platform-browser';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-detail-view',
@@ -17,61 +17,63 @@ import { SafeResourceUrl,DomSanitizer } from '@angular/platform-browser';
 
 export class DetailViewComponent implements OnInit {
     expenses;
-    docs: any[]=[];
-    pdfs:any[]=[];
+    docs: any[] = [];
+    pdfs: any[] = [];
     expense: Expense = new Expense();
     constructor(private router: Router,
         private route: ActivatedRoute,
         private expenseService: ExpenseService,
         private toastyService: ToastyService,
         private docService: DocsService,
-        private sanitizer: DomSanitizer) {
-        
+        private sanitizer: DomSanitizer, @Inject('BASE_URL') private originUrl: string) {
+
         route.params.subscribe(p => {
             this.expense.expenseId = +p['id'];
         });
     }
 
     ngOnInit() {
-       this.docService.getDocs(this.expense.expenseId)
+        this.docService.getDocs(this.expense.expenseId)
             .subscribe(doc => {
-                for(var i=0;i<doc[0].length;i++){
-                if (doc[0][i].docName.endsWith(".pdf")) {
-                    //PDFs Collection
-                    doc[0][i].docName = this.sanitizer.bypassSecurityTrustResourceUrl('/uploads/'+doc[0][i].docName);
-                    this.pdfs.push(doc[0][i]);
-                    console.log("PDFs:- ", this.pdfs);
-                } else {
-                    //Images Collection
-                    this.docs.push(doc[0][i]);
+                for (var i = 0; i < doc[0].length; i++) {
+                    if (doc[0][i].docName.endsWith(".pdf")) {
+                        //PDFs Collection
+                        doc[0][i].docName = this.sanitizer.bypassSecurityTrustResourceUrl(this.originUrl + '/uploads/' + doc[0][i].docName);
+                        this.pdfs.push(doc[0][i]);
+
+                    } else {
+                        //Images Collection
+                        doc[0][i].docName = this.sanitizer.bypassSecurityTrustResourceUrl(this.originUrl + '/uploads/' + doc[0][i].docName);
+                        this.docs.push(doc[0][i]);
+
                     }
                 }
-                console.log("docs", this.docs);
-            },err => {
+
+            }, err => {
                 console.log("Error Occured while fetching docs!");
             });
         this.expenseService.getExpenseById(this.expense.expenseId)
             .subscribe(e => {
-                //Since, I am sending the component back as querable data from server.
-                //But, in case of ID, it will be only one.
-                this.expenses = e[0];
-                this.toastyService.success({
-                    title: 'Success',
-                    msg: 'Expense retieved successfully!',
-                    theme: 'bootstrap',
-                    showClose: true,
-                    timeout: 5000
+                    //Since, I am sending the component back as querable data from server.
+                    //But, in case of ID, it will be only one.
+                    this.expenses = e[0];
+                    this.toastyService.success({
+                        title: 'Success',
+                        msg: 'Expense retieved successfully!',
+                        theme: 'bootstrap',
+                        showClose: true,
+                        timeout: 5000
+                    });
+                },
+                err => {
+                    this.toastyService.error({
+                        title: 'Error',
+                        msg: 'Error Occured while Fetching Expenses!',
+                        theme: 'bootstrap',
+                        showClose: true,
+                        timeout: 5000
+                    });
                 });
-            },
-            err => {
-                this.toastyService.error({
-                    title: 'Error',
-                    msg: 'Error Occured while Fetching Expenses!',
-                    theme: 'bootstrap',
-                    showClose: true,
-                    timeout: 5000
-                });
-            });
     }
 
 
